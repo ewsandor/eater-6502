0edae1809996891e223ab29bf5b907a4cb8d2d0a
Edward Sandor <public+git@sandorlaboratories.com>
2023-09-09 13:33:43 -0500

    Fix comments

--- nop_then_halt.s
+++ nop_then_halt.s
@@ -1,4 +1,4 @@
-; Walk through EEPROM padded with NOPs 255*255 times, then halt
+; Walk through EEPROM padded with NOPs 255 times (~16 seconds at 1MHz), then halt
 
 
 
@@ -26,11 +26,11 @@ nop_loop:
   ; ... (nop) ...
   ; ... (nop) ...
   ; ... (nop) ...
-  .org (NMIB_VECTOR-9) ; Assume loop handler is 15-bytes (from VASM output)
+  .org (NMIB_VECTOR-9) ; Assume loop handler is 9-bytes (from VASM output)
 
   dex ; Decrement x
-  ; If x > 0, jump back to NOP loop when end of EEPROM is reached.  If x == 0, halt
-  bne nop_loop_jmp ; Branch uses relative addressing. Origin beyond max relative address, branch to near by jmp instruction
+  ; If x > 0, jump back to top of NOP loop when end of EEPROM is reached.  If x == 0, halt
+  bne nop_loop_jmp ; Branch uses relative addressing. EEPROM origin beyond max relative address, branch to near by jmp instruction
   jmp halt ; End of program
 
 nop_loop_jmp:
