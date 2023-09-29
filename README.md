f1fdaf96bb11d1e3bc540d2b76126e465b3ad2b9
Edward Sandor <public+git@sandorlaboratories.com>
2023-09-29 13:30:10 -0500

    Option to throttle WOZMON input

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Example build command: `vasm6502_oldstyle -Fbin -dotdir -wdc02 hello_world.s`
 
 Example EEPROM write command: `minipro -p AT28C256 -w a.out`
 
+Example WOZMON load command: `script/load_bin_to_wozmon.py -t -o 0x0800 a.out > /dev/ttyUSB0`
 
 ## Resources
 - [Ben Eater's 6502 Computer Kit](https://eater.net/6502)
--- script/load_bin_to_wozmon.py
+++ script/load_bin_to_wozmon.py
@@ -1,6 +1,7 @@
 #!/bin/python3
 # Basic script to dump binary file to WOZMON
 import argparse
+from time import sleep
 
 # Wrapper function to parse int arguments with literal prefixes
 def int_literal(str):
@@ -14,7 +15,8 @@ def cr_flush():
 argument_parser = argparse.ArgumentParser()
 argument_parser.add_argument("bin", type=argparse.FileType("rb"), help="Path to binary file to be loaded to WOZMON.")
 argument_parser.add_argument("-n", "--newline", action='store_const', const = '\r\n', default='\r', help="Include newline character in line endings for compatibility with modern terminal formatting (defaults to only CR for WOZMON).")
-argument_parser.add_argument("-o", "--origin", type=int_literal, default=0, help="Memory address origin to load binary at.")
+argument_parser.add_argument("-o", "--origin", type=int_literal, default=0, help="Memory-address origin to load binary at.")
+argument_parser.add_argument("-t", "--throttle", action='store_true', help="Throttle output to avoid overflowing WOZMON device's input buffer.")
 args = argument_parser.parse_args()
 
 # Prepare variables for writing
@@ -32,9 +34,11 @@ for bin_byte in bin_byte_array:
     # Clear "first byte" flag
     first_byte = False
   # Output next byte
-  print(" " + format(bin_byte, "02X"), end='')
+  print(" " + format(bin_byte, "02X"), end='', flush=True)
   # Increment write address
   byte_address = byte_address+1
+  if args.throttle:
+    sleep(0.005)
 
 # Send CR return to flush any pending input
 cr_flush()
